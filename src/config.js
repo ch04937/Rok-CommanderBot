dcdbd55456c03aca3f967125043ba77e56833b76
ch04937 <97hernandez.c@gmail.com>
2019-11-01 16:07:20 -0700

    adding images assets, and setup

--- src/config.js
+++ src/config.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 
 module.exports = {
-    BOT_TOKEN:'zo1MrYV2ZdQCyeHaRENgkFIehtnbqJnA',
+    BOT_TOKEN:'NjM5MTcyMjM0MzIxMTk5MTE4.Xbtz6g.XliOkg9coqta2qJGuUVOmmsMDXo',
+    PREFIX:'!',
+    COMMANDS:[
+        
+    ]
     
 }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+
+const { Client, MessageAttachment } = require('discord.js')
+const client = new Client({ disableEveryone: true });
 
 const {
-    BOT_TOKEN,
+    BOT_TOKEN,    
+} = require('./config.js')
 
-}
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`);
+    console.log(` \n Logged in as ${client.user.username} \n`);
 })
 
+// playing ping pong
 client.on('message', msg => {
-    if(msg.content === 'ping') {
+    if(msg.content === '!ping') {
         msg.reply('Pong!');
     }
 });
+
+//image basic image
+client.on('message', msg => {
+    if (msg.content === "!herman") {
+        msg.channel.send('herman', {
+            files: [
+                "./src/assets/herman1.PNG", 
+                "./src/assets/herman2.PNG",
+                "./src/assets/herman3.PNG",
+
+            ]
+        })
+    }
+})
+
 
 client.login(BOT_TOKEN)
