2d9bd631588331bffe9db6b266aa134eed4ba12b
ch04937 <97hernandez.c@gmail.com>
2019-11-06 17:10:28 -0800

    fixed prefix

--- src/commands/sunQoute.js
+++ src/commands/sunQoute.js
@@ -1,14 +1,14 @@
 const sunTzu = require('sun-tzu-quotes')
 
-const fire = require('../config.json')
+const {quote} = require('../config.json')
 
 module.exports = {
     name: 'sun tzu quotes',
-    triggers: [`${fire}wise`],
+    triggers: [`${quote}wise`],
     descrition: 'get a famous quote fom sun tzu',
     handler: (message) => {
         if(message.author.bot) return; // checks if message was sent by a bot
-        if(!message.content.startsWith(fire)) return; //makes so that user needs prefix
+        if(!message.content.startsWith(quote)) return; //makes so that user needs prefix
 
         return message.channel.send(sunTzu())
     }
