f91765ab7693faec0ea964c3c4686a5183f39472
ch04937 <97hernandez.c@gmail.com>
2019-11-09 12:32:31 -0800

    patches

--- src/commands/sunQoute.js
+++ src/commands/sunQoute.js
@@ -1,15 +1,11 @@
 const sunTzu = require('sun-tzu-quotes')
 
-const { quote } = require('../config.json')
-
 module.exports = {
     name: 'Sun Tzu Quotes',
     triggers: ['wise'],
     descrition: 'get a famous quote fom sun tzu',
     handler: (message) => {
         if(message.author.bot) return; // checks if message was sent by a bot
-        if(!message.content.startsWith(quote)) return; //makes so that user needs prefix
-
         return message.channel.send(sunTzu())
     }
 }
