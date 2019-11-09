2138b81f5d96b51cf8749b4227262044e79eb678
ch04937 <97hernandez.c@gmail.com>
2019-11-09 12:12:05 -0800

    delete starts with to make commands accurate

--- src/commands/upgrade.js
+++ src/commands/upgrade.js
@@ -17,79 +17,79 @@ module.exports = {
 
         if(message.author.bot) return; // checks if message was sent by a bot
         if(!message.content.startsWith(prefix)) return; //makes so that user needs prefix
-        if ( msg.startsWith(prefix+'1')) {
+        if ( msg === prefix+'1') {
             msgs.send({ embed: building.city1 })
         }
-        if ( msg.startsWith(prefix+'2')) {
+        if ( msg === prefix+'2') {
             msgs.send({ embed: building.city2 })
         }
-        if ( msg.startsWith(prefix+'3')) {
+        if ( msg === prefix+'3') {
             msgs.send({ embed: building.city3 })
         }
-        if ( msg.startsWith(prefix+'4')) {
+        if ( msg === prefix+'4') {
             msgs.send({ embed: building.city4 })
         }
-        if ( msg.startsWith(prefix+'5')) {
+        if ( msg === prefix+'5') {
             msgs.send({ embed: building.city5 })
         }
-        if ( msg.startsWith(prefix+'6')) {
+        if ( msg === prefix+'6') {
             msgs.send({ embed: building.city6 })
         }
-        if ( msg.startsWith(prefix+'7')) {
+        if ( msg === prefix+'7') {
             msgs.send({ embed: building.city7 })
         }
-        if ( msg.startsWith(prefix+'8')) {
+        if ( msg === prefix+'8') {
             msgs.send({ embed: building.city8 })
         }
-        if ( msg.startsWith(prefix+'9')) {
+        if ( msg === prefix+'9') {
             msgs.send({ embed: building.city9 })
         }
-        if ( msg.startsWith(prefix+'10')) {
+        if ( msg === prefix+'10') {
             msgs.send({ embed: building.city10 })
         }
-        if ( msg.startsWith(prefix+'11')) {
+        if ( msg === prefix+'11') {
             msgs.send({ embed: building.city11 })
         }
-        if ( msg.startsWith(prefix+'12')) {
+        if ( msg === prefix+'12') {
             msgs.send({ embed: building.city12 })
         }
-        if ( msg.startsWith(prefix+'13')) {
+        if ( msg === prefix+'13') {
             msgs.send({ embed: building.city13 })
         }
-        if ( msg.startsWith(prefix+'14')) {
+        if ( msg === prefix+'14') {
             msgs.send({ embed: building.city14 })
         }
-        if ( msg.startsWith(prefix+'15')) {
+        if ( msg === prefix+'15') {
             msgs.send({ embed: building.city15 })
         }
-        if ( msg.startsWith(prefix+'16')) {
+        if ( msg === prefix+'16') {
             msgs.send({ embed: building.city16 })
         }
-        if ( msg.startsWith(prefix+'17')) {
+        if ( msg === prefix+'17') {
             msgs.send({ embed: building.city17 })
         }
-        if ( msg.startsWith(prefix+'18')) {
+        if ( msg === prefix+'18') {
             msgs.send({ embed: building.city18 })
         }
-        if ( msg.startsWith(prefix+'19')) {
+        if ( msg === prefix+'19') {
             msgs.send({ embed: building.city19 })
         }
-        if ( msg.startsWith(prefix+'20')) {
+        if ( msg === prefix+'20') {
             msgs.send({ embed: building.city20 })
         }
-        if ( msg.startsWith(prefix+'21')) {
+        if ( msg === prefix+'21') {
             msgs.send({ embed: building.city21 })
         }
-        if ( msg.startsWith(prefix+'22')) {
+        if ( msg === prefix+'22') {
             msgs.send({ embed: building.city22 })
         }
-        if ( msg.startsWith(prefix+'23')) {
+        if ( msg === prefix+'23') {
             msgs.send({ embed: building.city23 })
         }
-        if ( msg.startsWith(prefix+'24')) {
+        if ( msg === prefix+'24') {
             msgs.send({ embed: building.city24 })
         }
-        if ( msg.startsWith(prefix+'25')) {
+        if ( msg === prefix+'25') {
             msgs.send({ embed: building.city25 })
         }
     })
